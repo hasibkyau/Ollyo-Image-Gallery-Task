55e1447e64979f15169c9c32e2b5ea1a642dab99
Md. Hasibur Rahman <hasibkyau.cse@gmail.com>
2023-11-06 10:29:40 +0600

    added functions for moving and updating image to a new position

--- src/App.js
+++ src/App.js
@@ -1,155 +1,153 @@
-// import "./App.css";
-// import Gallery from "./components/gallery/gallery";
+import "./App.css";
+import Gallery from "./components/gallery/gallery";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <div className="main-container">
-//         <Gallery/>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-import React, { useState } from "react";
-import { useDrag, useDrop } from "react-dnd";
-import './App.css';
-
-import galleryList from "./data.js";
-
-
-
-const Card = ({ src, title, id, index, moveImage, handleCallback }) => {
-  const ref = React.useRef(null);
-  const checkboxRef = React.useRef(null);
-
-  const [, drop] = useDrop({
-    accept: "image",
-    hover: (item, monitor) => {
-      if (!ref.current) {
-        return;
-      }
-      const dragIndex = item.index;
-      const hoverIndex = index;
-
-      if (dragIndex === hoverIndex) {
-        return;
-      }
-
-      const hoverBoundingRect = ref.current?.getBoundingClientRect();
-
-      const hoverMiddleY =
-        (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
-
-      const clientOffset = monitor.getClientOffset();
-      const hoverClientY = clientOffset.y - hoverBoundingRect.top;
-
-      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
-        return;
-      }
-
-      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
-        return;
-      }
-
-      moveImage(dragIndex, hoverIndex);
-
-      item.index = hoverIndex;
-    },
-  });
-
-  const [{ isDragging }, drag] = useDrag({
-    type: "image",
-    item: () => {
-      return { id, index };
-    },
-    collect: (monitor) => {
-      return {
-        isDragging: monitor.isDragging(),
-      };
-    },
-  });
-
-  const opacity = isDragging ? 0 : 1;
-  drag(drop(ref));
-
-  // const isChecked = checkboxRef.current?.checked;
-  // console.log(isChecked);
-
+function App() {
   return (
-    <div ref={ref} style={{ opacity }} className={"item item-" + index}>
-      <input
-        ref={checkboxRef}
-        className="checkbox"
-        type="checkbox"
-        onChange={handleCallback}
-      />
-      <img src={src} alt={title} />
+    <div className="App">
+        <Gallery/>
     </div>
   );
-};
-
-const App = () => {
-
-  const [Count, setCount] = useState(0);   
-  const [images, setImages] = React.useState(galleryList);
-
-  const moveImage = React.useCallback((dragIndex, hoverIndex) => {
-    setImages((prevCards) => {
-      const clonedCards = [...prevCards];
-      const removedItem = clonedCards.splice(dragIndex, 1)[0];
-
-      clonedCards.splice(hoverIndex, 0, removedItem);
-      return clonedCards;
-    });
-  }, []);
-
-  // Callback function to handle data received from the
-  const handleCallback = (childData) => {
-    let value = childData.target.checked;
-    if (value) {
-      setCount(Count + 1);
-      childData.target.className = "active";
-    } else {
-      if (Count > 0) {
-        setCount(Count - 1);
-        childData.target.className = "checkbox";
-      }
-    }
-    console.log("Total Image:", Count);
-  };
-
-  return (
-    <main>
-      <div className="container">
-        <div className="top-left-bar">
-          <p>{Count ? Count + " File Selected" : "Gallery"}</p>
-        </div>
-        <div className="top-right-bar">
-          <p>{Count ? "Delete files" : ""}</p>
-        </div>
-        {React.Children.toArray(
-          images.map((image, index) => (
-            <Card
-              className="card"
-              src={image.img}
-              title={image.title}
-              id={image.id}
-              index={index}
-              moveImage={moveImage}
-              handleCallback={handleCallback}
-            />
-          ))
-        )}
-        <div className="add-images">
-          <img src="/images/image-12.png" alt="add more" />
-          <p>Add Images</p>
-        </div>
-      </div>
-    </main>
-  );
-};
+}
 
 export default App;
+
+// import React, { useState } from "react";
+// import { useDrag, useDrop } from "react-dnd";
+// import './App.css';
+
+// import galleryList from "./data.js";
+
+
+
+// const Card = ({ src, title, id, index, moveImage, handleCallback }) => {
+//   const ref = React.useRef(null);
+//   const checkboxRef = React.useRef(null);
+
+//   const [, drop] = useDrop({
+//     accept: "image",
+//     hover: (item, monitor) => {
+//       if (!ref.current) {
+//         return;
+//       }
+//       const dragIndex = item.index;
+//       const hoverIndex = index;
+
+//       if (dragIndex === hoverIndex) {
+//         return;
+//       }
+
+//       const hoverBoundingRect = ref.current?.getBoundingClientRect();
+
+//       const hoverMiddleY =
+//         (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+
+//       const clientOffset = monitor.getClientOffset();
+//       const hoverClientY = clientOffset.y - hoverBoundingRect.top;
+
+//       if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+//         return;
+//       }
+
+//       if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+//         return;
+//       }
+
+//       moveImage(dragIndex, hoverIndex);
+
+//       item.index = hoverIndex;
+//     },
+//   });
+
+//   const [{ isDragging }, drag] = useDrag({
+//     type: "image",
+//     item: () => {
+//       return { id, index };
+//     },
+//     collect: (monitor) => {
+//       return {
+//         isDragging: monitor.isDragging(),
+//       };
+//     },
+//   });
+
+//   const opacity = isDragging ? 0 : 1;
+//   drag(drop(ref));
+
+//   // const isChecked = checkboxRef.current?.checked;
+//   // console.log(isChecked);
+
+//   return (
+//     <div ref={ref} style={{ opacity }} className={"item item-" + index}>
+//       <input
+//         ref={checkboxRef}
+//         className="checkbox"
+//         type="checkbox"
+//         onChange={handleCallback}
+//       />
+//       <img src={src} alt={title} />
+//     </div>
+//   );
+// };
+
+// const App = () => {
+
+//   const [Count, setCount] = useState(0);   
+//   const [images, setImages] = React.useState(galleryList);
+
+//   const moveImage = React.useCallback((dragIndex, hoverIndex) => {
+//     setImages((prevCards) => {
+//       const clonedCards = [...prevCards];
+//       const removedItem = clonedCards.splice(dragIndex, 1)[0];
+
+//       clonedCards.splice(hoverIndex, 0, removedItem);
+//       return clonedCards;
+//     });
+//   }, []);
+
+//   // Callback function to handle data received from the
+//   const handleCallback = (childData) => {
+//     let value = childData.target.checked;
+//     if (value) {
+//       setCount(Count + 1);
+//       childData.target.className = "active";
+//     } else {
+//       if (Count > 0) {
+//         setCount(Count - 1);
+//         childData.target.className = "checkbox";
+//       }
+//     }
+//     console.log("Total Image:", Count);
+//   };
+
+//   return (
+//     <main>
+//       <div className="container">
+//         <div className="top-left-bar">
+//           <p>{Count ? Count + " File Selected" : "Gallery"}</p>
+//         </div>
+//         <div className="top-right-bar">
+//           <p>{Count ? "Delete files" : ""}</p>
+//         </div>
+//         {React.Children.toArray(
+//           images.map((image, index) => (
+//             <Card
+//               className="card"
+//               src={image.img}
+//               title={image.title}
+//               id={image.id}
+//               index={index}
+//               moveImage={moveImage}
+//               handleCallback={handleCallback}
+//             />
+//           ))
+//         )}
+//         <div className="add-images">
+//           <img src="/images/image-12.png" alt="add more" />
+//           <p>Add Images</p>
+//         </div>
+//       </div>
+//     </main>
+//   );
+// };
+
+// export default App;
--- src/components/gallery/gallery.js
+++ src/components/gallery/gallery.js
@@ -1,138 +1,69 @@
 import React, { useState } from "react";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import galleryList from "../../data.js";
+import Card from "../card/card.js";
 import "./gallery.css";
 
-const finalSpaceCharacters = [
-  {
-    id: "1",
-    name: "product",
-    thumb: "/images/image-1.webp",
-  },
-  {
-    id: "2",
-    name: "product",
-    thumb: "/images/image-2.webp",
-  },
-  {
-    id: "3",
-    name: "product",
-    thumb: "/images/image-3.webp",
-  },
-  {
-    id: "4",
-    name: "product",
-    thumb: "/images/image-4.webp",
-  },
-  {
-    id: "5",
-    name: "product",
-    thumb: "/images/image-5.webp",
-  },
-  {
-    id: "6",
-    name: "product",
-    thumb: "/images/image-6.webp",
-  },
-  {
-    id: "7",
-    name: "product",
-    thumb: "/images/image-7.webp",
-  },
-  {
-    id: "8",
-    name: "product",
-    thumb: "/images/image-8.webp",
-  },
-  {
-    id: "9",
-    name: "product",
-    thumb: "/images/image-9.webp",
-  },
-  {
-    id: "10",
-    name: "product",
-    thumb: "/images/image-10.jpeg",
-  },
-  {
-    id: "11",
-    name: "product",
-    thumb: "/images/image-11.jpeg",
-  },
-  {
-    id: "12",
-    name: "product",
-    thumb: "/images/image-12.png",
-  },
-];
+const Gallery = () => {
+  const [Count, setCount] = useState(0);
+  const [images, setImages] = React.useState(galleryList);
 
-function Gallery() {
-  const [products, updateCharacters] = useState(finalSpaceCharacters);
+  //Function for moving image by reordering the index
+  const moveImage = React.useCallback((dragIndex, hoverIndex) => {
+    setImages((prevCards) => {
+      const clonedCards = [...prevCards];
+      //exchanging the index
+      const removedItem = clonedCards.splice(dragIndex, 1)[0];
+      clonedCards.splice(hoverIndex, 0, removedItem);
+      return clonedCards;
+    });
+  }, []);
 
-  function handleOnDragEnd(result) {
-    if (!result.destination) return;
-
-    const items = Array.from(products);
-    const [reorderedItem] = items.splice(result.source.index, 1);
-    items.splice(result.destination.index, 0, reorderedItem);
-    console.log("Update items:", items);
-
-    updateCharacters(items);
-  }
+  // Callback function to handle data received from the Card Component
+  const handleCallback = (childData) => {
+    //Count total selected images
+    let value = childData.target.checked;
+    if (value) {
+      setCount(Count + 1);
+      //if there is one or more than one image is selected, the checkbox will be visible
+      childData.target.className = "active";
+    } else {
+      if (Count > 0) {
+        setCount(Count - 1);
+        //if there is no image select the checkbox class will hide the checkbox
+        childData.target.className = "deactive";
+      }
+    }
+  };
 
   return (
-    <div className="Gallery">
-      <header className="Gallery-header">
-        <h1>Final Space Characters</h1>
-        <DragDropContext onDragEnd={handleOnDragEnd}>
-          <Droppable droppableId="products">
-            {(provided) => (
-              <ul
-                className="products"
-                {...provided.droppableProps}
-                ref={provided.innerRef}
-              >
-                {products.map(({ id, name, thumb }, index) => {
-                  if (id != 12) {
-                    return (
-                      <Draggable key={id} draggableId={id} index={index}>
-                        {(provided) => (
-                          <li
-                            ref={provided.innerRef}
-                            {...provided.draggableProps}
-                            {...provided.dragHandleProps}
-                            className={"item-" + index}
-                          >
-                            <div className="products-thumb items">
-                              <img src={thumb} alt={`${name} Thumb`} />
-                            </div>
-                          </li>
-                        )}
-                      </Draggable>
-                    );
-                  } else {
-                    return (
-                      <li>
-                        <div className="products-thumb items">
-                          <img src={thumb} alt={`${name} Thumb`} />
-                        </div>
-                      </li>
-                    );
-                  }
-                })}
-                {provided.placeholder}
-              </ul>
-            )}
-          </Droppable>
-        </DragDropContext>
-      </header>
-      <p>
-        Images from{" "}
-        <a href="https://final-space.fandom.com/wiki/Final_Space_Wiki">
-          Final Space Wiki
-        </a>
-      </p>
-    </div>
+    <main className="main-container">
+      <div className="submain-container">
+        <div className="top-left-bar">
+          <p>{Count ? Count + " File Selected" : "Gallery"}</p>
+        </div>
+        <div className="top-right-bar">
+          <p>{Count ? "Delete files" : ""}</p>
+        </div>
+        {React.Children.toArray(
+          images.map((image, index) => (
+            <Card
+              className="card"
+              src={image.img}
+              title={image.title}
+              id={image.id}
+              index={index}
+              moveImage={moveImage}
+              handleCallback={handleCallback}
+            />
+          ))
+        )}
+        <div className="bottom-card">
+          <img src="/images/image-12.png" alt="add more" />
+          <p>Add Images</p>
+        </div>
+      </div>
+    </main>
   );
-}
+};
 
 export default Gallery;
